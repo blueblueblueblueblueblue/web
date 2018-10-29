721ffa279b71304332d24868c5bd577f2b02640a
liyaoxin <liyaoxin1314@foxmail.com>
2018-10-29 23:02:29 +0800

    xiugai

--- src/components/Dataformat.vue
+++ src/components/Dataformat.vue
@@ -7,9 +7,14 @@
         <el-breadcrumb-item>数据格式化</el-breadcrumb-item>
       </el-breadcrumb>
     </el-col>
-    <el-col :span="24">
+    <el-col :span="24" style="margin-left: 20px">
+      <h4>数据表项:</h4>
+    </el-col>
+
+    <el-col :span="17" class="dataitem">
 
     </el-col>
+    <el-col :span="5" class="dataitembtn"></el-col>
   </el-row>
 </template>
 
@@ -20,5 +25,17 @@
 </script>
 
 <style scoped>
+.dataitem{
+  height: 500px;
+  border: 1px dashed #00C1DE;
+  margin-right: 20px;
+  margin-left: 20px;
 
+}
+.dataitembtn{
+  height: 500px;
+  border: 1px dashed #de6a19;
+  margin-left: 20px;
+
+}
 </style>
